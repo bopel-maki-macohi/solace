569d20ca0b786f999783d5c91c083444c60481c3
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-21 13:16:35 -0500

    watcher is just the icon

--- source/solace/PlayState.hx
+++ source/solace/PlayState.hx
@@ -6,20 +6,12 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
-	var watcher:FlxSprite;
-	var watcherPatience:Int = 10000;
-
 	var pickaxde:SolaceSprite;
 	var shovil:SolaceSprite;
 
 	override function create()
 	{
 		super.create();
-
-		watcher = new FlxSprite('assets/watcher/happy.png');
-		add(watcher);
-		watcher.screenCenter();
-		watcher.x = watcher.width / -2;
 
 		add(pickaxde = new SolaceSprite('assets/tools/pickaxe.png'));
 		pickaxde.scaleTo(2, 2);
@@ -35,7 +27,5 @@ class PlayState extends FlxState
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
-
-		watcherPatience--;
 	}
 }
