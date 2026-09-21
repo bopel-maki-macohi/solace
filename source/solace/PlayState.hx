8af998337f226f19e29978ea5dd175bf0b697f05
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-21 13:21:47 -0500

    make cash moneyy

--- source/solace/PlayState.hx
+++ source/solace/PlayState.hx
@@ -1,5 +1,7 @@
 package solace;
 
+import solace.save.SaveFlags;
+import flixel.text.FlxText;
 import solace.flixel.SolaceSprite;
 import flixel.FlxSprite;
 import flixel.FlxState;
@@ -9,23 +11,44 @@ class PlayState extends FlxState
 	var pickaxde:SolaceSprite;
 	var shovil:SolaceSprite;
 
+	var cashmonee:FlxText;
+
 	override function create()
 	{
 		super.create();
 
-		add(pickaxde = new SolaceSprite('assets/tools/pickaxe.png'));
+		add(pickaxde = new SolaceSprite('assets/tools/pickaxe.png').setInteractable());
 		pickaxde.scaleTo(2, 2);
 		pickaxde.screenCenter();
 		pickaxde.x -= pickaxde.width;
 
-		add(shovil = new SolaceSprite('assets/tools/shovel.png'));
+		add(shovil = new SolaceSprite('assets/tools/shovel.png').setInteractable());
 		shovil.scaleTo(2, 2);
 		shovil.screenCenter();
 		shovil.x += shovil.width;
+
+		add(cashmonee = new FlxText(0, 0, 0, 'CASH MONEY', 16));
+		cashmonee.screenCenter(X);
+		cashmonee.y = cashmonee.size;
+
+		pickaxde.onLeftClick.add(onPickClick);
+		shovil.onLeftClick.add(onShovelClick);
+	}
+
+	function onPickClick()
+	{
+		SaveFlags.MONEY.value += 2 / 10;
+	}
+
+	function onShovelClick()
+	{
+		SaveFlags.MONEY.value += 1 / 10;
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		cashmonee.text = 'Money: ${SaveFlags.MONEY.value}';
 	}
 }
