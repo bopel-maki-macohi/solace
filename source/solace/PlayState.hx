2f9feac645edc926d55d5a7bd1889174135b4ffc
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-21 13:15:48 -0500

    pickaxe and shovel sprites

--- source/solace/PlayState.hx
+++ source/solace/PlayState.hx
@@ -1,5 +1,6 @@
 package solace;
 
+import solace.flixel.SolaceSprite;
 import flixel.FlxSprite;
 import flixel.FlxState;
 
@@ -7,6 +8,9 @@ class PlayState extends FlxState
 {
 	var watcher:FlxSprite;
 	var watcherPatience:Int = 10000;
+
+	var pickaxde:SolaceSprite;
+	var shovil:SolaceSprite;
 
 	override function create()
 	{
@@ -16,9 +20,20 @@ class PlayState extends FlxState
 		add(watcher);
 		watcher.screenCenter();
 		watcher.x = watcher.width / -2;
+
+		add(pickaxde = new SolaceSprite('assets/tools/pickaxe.png'));
+		pickaxde.scaleTo(2, 2);
+		pickaxde.screenCenter();
+		pickaxde.x -= pickaxde.width;
+
+		add(shovil = new SolaceSprite('assets/tools/shovel.png'));
+		shovil.scaleTo(2, 2);
+		shovil.screenCenter();
+		shovil.x += shovil.width;
 	}
 
-	override function update(elapsed:Float) {
+	override function update(elapsed:Float)
+	{
 		super.update(elapsed);
 
 		watcherPatience--;
---
+++ source/solace/flixel/SolaceSprite.hx
@@ -0,0 +1,44 @@
+package solace.flixel;
+
+import flixel.FlxG;
+import flixel.util.FlxSignal;
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+
+class SolaceSprite extends FlxSprite
+{
+	public var interactable:Bool = false;
+
+	public function setInteractable(?set:Null<Bool>):SolaceSprite
+	{
+		this.interactable = set ?? true;
+		return this;
+	}
+
+	public var onLeftClick:FlxSignal = new FlxSignal();
+	public var onMiddleClick:FlxSignal = new FlxSignal();
+	public var onRightClick:FlxSignal = new FlxSignal();
+
+	overload public extern inline function scaleTo(z:Float) scaleTo(z, z);
+
+	overload public extern inline function scaleTo(x:Float, y:Float)
+	{
+		this.scale.set(x, y);
+		updateHitbox();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (interactable && FlxG.mouse.overlaps(this))
+		{
+			if (FlxG.mouse.justPressed && onLeftClick != null) onLeftClick.dispatch();
+			if (FlxG.mouse.justPressedMiddle && onMiddleClick != null) onMiddleClick.dispatch();
+			if (FlxG.mouse.justPressedRight && onRightClick != null) onRightClick.dispatch();
+		}
+	}
+
+	override function makeGraphic(width:Int, height:Int, color:FlxColor = FlxColor.WHITE, unique:Bool = false,
+			?key:String):SolaceSprite return cast super.makeGraphic(width, height, color, unique, key);
+}
